c42492c8f34a58eeea9a3a4b29012fae1c0117eb
Swap <swapnilbelorkar@gmail.com>
2024-05-29 00:29:57 +0100

    adding navigation bar angular material

--- apps/swap-workspace/src/app/app.component.ts
+++ apps/swap-workspace/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NavComponent],
   selector: 'swap-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
